85b267024b42f1c51029954740aa50df1edc9f07
BenjDW <dewitskibenji@live.fr>
2025-03-21 13:08:22 -0700

    start , add main_subject

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 12:15:19 by bde-wits          #+#    #+#             */
+/*   Updated: 2025/03/21 12:53:52 by bde-wits         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include "ostream"
+#include "iostream"
+#include "exception"
+
+// Implémentez une classe MutantStack. Elle sera implémentée en termes de la std::stack. 
+// Elle offrira toutes ses fonctions membres avec en plus des itérateurs.
+
+class MutantStack
+{
+	public:
+		MutantStack();
+		~MutantStack();
+	private:
+};
+
+MutantStack::MutantStack()
+{
+}
+
+MutantStack::~MutantStack()
+{
+}

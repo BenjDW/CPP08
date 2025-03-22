991004810c18eb9db8e9ebc98f890ea4457d8a2b
BenjDW <dewitskibenji@live.fr>
2025-03-22 06:09:57 -0700

    add the rest of iterator in container for the stack, add test list in main and canonical class

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -6,18 +6,19 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 12:15:19 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/21 14:01:14 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/22 06:08:24 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
-#include "ostream"
-#include "iostream"
-#include "exception"
+#include <ostream>
+#include <iostream>
+#include <exception>
 #include <stack>
 #include <deque>
 #include <iostream>
 #include <iterator>
+#include <list>
 
 // Implémentez une classe MutantStack. Elle sera implémentée en termes de la std::stack. 
 // Elle offrira toutes ses fonctions membres avec en plus des itérateurs.
@@ -29,10 +30,18 @@ class MutantStack : public std::stack<T>
 		MutantStack();
 		MutantStack(const MutantStack &cpy);
 		~MutantStack();
-		typedef typename std::stack<T>::container_type::iterator iterator;
-		typedef typename std::stack<T>::container_type::reverse_iterator reverse_iterator;
-		iterator	begin();
-		iterator	end();
+		typedef typename std::stack<T>::container_type::iterator 				iterator;
+		typedef	typename std::stack<T>::container_type::const_iterator 			const_iterator;
+		typedef typename std::stack<T>::container_type::reverse_iterator 		reverse_iterator;
+		typedef typename std::stack<T>::container_type::const_reverse_iterator 	const_reverse_iterator;
+		iterator				begin();
+		iterator				end();
+		const_iterator			cbegin();
+		const_iterator			cend();
+		reverse_iterator		rbegin();
+		reverse_iterator		rend();
+		const_reverse_iterator	crbegin();
+		const_reverse_iterator	crend();
 		MutantStack operator=(MutantStack &cpy);
 	private:
 };
@@ -49,6 +58,41 @@ typename MutantStack<T>::iterator MutantStack<T>::end()
 	return (this->c.end());
 }
 
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::cbegin() 
+{
+    return (this->c.cbegin());
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::cend()
+{
+	return (this->c.cend());
+}
+
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rbegin() 
+{
+    return (this->c.rbegin());
+}
+
+template <typename T>
+typename MutantStack<T>::reverse_iterator MutantStack<T>::rend()
+{
+	return (this->c.rend());
+}
+
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::crbegin() 
+{
+    return (this->c.rbegin());
+}
+
+template <typename T>
+typename MutantStack<T>::const_reverse_iterator MutantStack<T>::crend()
+{
+	return (this->c.rend());
+}
 
 template <typename T>
 MutantStack<T>::MutantStack() : std::stack<T>()
@@ -59,3 +103,16 @@ template <typename T>
 MutantStack<T>::~MutantStack()
 {
 }
+
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack &cpy) : std::stack<T>(cpy)
+{
+}
+
+template <typename T>
+MutantStack<T> MutantStack<T>::operator=(MutantStack &cpy)
+{
+	if (this != &cpy)
+		std::stack<T>::operator=(cpy);
+	return (*this);
+} 
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,11 +6,13 @@
 /*   By: bde-wits <bde-wits@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 12:15:27 by bde-wits          #+#    #+#             */
-/*   Updated: 2025/03/21 14:00:09 by bde-wits         ###   ########.fr       */
+/*   Updated: 2025/03/22 06:04:58 by bde-wits         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "MutantStack.hpp"
+
+// subject main :
 
 int main()
 {
@@ -21,7 +23,7 @@ int main()
 	mstack.pop();
 	std::cout << mstack.size() << std::endl;
 	mstack.push(3);
-	mstack.push(5);
+	mstack.push(7);
 	mstack.push(737);
 	//[...]
 	mstack.push(0);
@@ -29,11 +31,28 @@ int main()
 	MutantStack<int>::iterator ite = mstack.end();
 	++it;
 	--it;
+	std::cout << std::endl;
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
 	std::stack<int> s(mstack);
+
+	// test with the list container
+	std::list<int> lst;
+	lst.push_back(5);
+	lst.push_back(3);
+	lst.push_back(7);
+	lst.push_back(737);
+	lst.push_back(0);
+
+	std::cout << "Iterate over std::list: ";
+	for (std::list<int>::iterator it = lst.begin(); it != lst.end(); ++it)
+	{
+		std::cout << *it << " ";
+	}
+	std::cout << std::endl;
+
 	return 0;
 }
